f2aded917088d7c6e5f5e7fce8f5a80cf1f1a00a
alexeybelsky.9000@gmail.com <paswordpomagimne12345>
2022-08-01 16:32:49 +0400

    Add modul 2

---
+++ Kartocka.py
@@ -0,0 +1,23 @@
+import time
+
+from selenium import  webdriver
+from selenium.webdriver.common.by import By
+
+webDriver = webdriver.Chrome(executable_path='C:\\Users\\User\\PycharmProjects\\pythonProject1\\chromedriver.exe')
+webDriver.get('https://www.saucedemo.com/')
+webDriver.maximize_window()
+
+
+
+webDriver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user') #Ввод логина
+
+webDriver.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce') #Ввод пароля
+
+
+webDriver.find_element(By.XPATH, '//*[@id="login-button"]').click()  #Клик по кнопке вход
+
+webDriver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div').click()   #Клик по карточке товара
+
+webDriver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()    #Клик добавить в корзину
+
+webDriver.find_element(By.XPATH, '//*[@id="back-to-products"]').click()     #Выход на главную
